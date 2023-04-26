8fe540ac8f78bb75517a5ee0871e14ff5ac24e1f
andy le <andy.le@cloud-kinetics.com>
2023-04-26 19:21:28 +0700

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,4 +1,4 @@
-# module "s3-bucket" {
-#   source  = "app.terraform.io/CK-LAB01/s3-bucket/aws"
-#   version = "2.8.0"
-# }
+module "s3-bucket" {
+  source  = "app.terraform.io/CK-LAB01/s3-bucket/aws"
+  version = "2.8.0"
+}
